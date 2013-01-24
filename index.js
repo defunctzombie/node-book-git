c9e8978e18ff61d613ef8e2b14cd582e07c9341f
Roman Shtylman <shtylman@gmail.com>
2013-01-24 03:22:52 -0500

    use option as parameter for gitdir

--- index.js
+++ index.js
@@ -3,17 +3,22 @@ module.exports = function(opt) {
 
     var git_id = null;
 
+    var cp_opt = {
+        cwd: opt || __dirname
+    };
+
+    var cmd = 'git rev-parse HEAD';
+
     // load git commit id
     // this is done async, which means anything logged before loaded won't
     // have a git id.
     // In practice this isn't much of a problem as the things we want to log are
     // generally not right at startup
-    require('child_process').exec('cd ' + __dirname + ' && git rev-parse HEAD',
-        function(err, stdout, stderr) {
+    require('child_process').exec(cmd, cp_opt, function(err, stdout, stderr) {
         if (err) {
-            return process.stderr.write(err + '\n');
+            process.stderr.write(err + '\n');
         }
-        git_id = stdout.trim();
+        git_id = stdout.trim() || stderr.trim();
     });
 
     // return the actual middleware
@@ -21,5 +26,4 @@ module.exports = function(opt) {
         var entry = this;
         entry.commit = git_id;
     }
-
 };
